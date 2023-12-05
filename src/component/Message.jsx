bcfa3a89c7fc487cf685a74ded3212ccadfdfc7e
tawheedshimul <tawheedshimul@gmail.com>
2023-12-06 01:42:10 +0600

    updae

--- src/component/Message.jsx
+++ src/component/Message.jsx
@@ -1,27 +1,28 @@
-// Message.js
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { AuthContex } from '../context/AuthContext';
 import { ChatContex } from '../context/ChatContext';
 
 function Message({ message }) {
-  const { currentUser } = useContext(AuthContex); 
+  const { currentUser } = useContext(AuthContex);
   const { data } = useContext(ChatContex);
 
-  
+  const messageRef = useRef();
+
+  useEffect(() => {
+    messageRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  }, [message]);
 
   return (
-    <div>
-      <div className={`flex gap-6 ${message.senderId === currentUser.uid ? 'justify-end' : 'justify-start'}`}>
-        <div className='max-w-4/5 flex flex-col gap-1 text-gray-300'>
-          <img className='h-10 w-10 rounded-full' src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt="" />
-          <span>Just now</span>
-        </div>
-        <div className='flex flex-col gap-3'>
-          <p className='bg-white p-2 rounded-tl-xl rounded-br-xl'>
-            {message.text}
-          </p>
-          {message.img && <img className='w-40 h-40 rounded object-fit' src={message.img} alt="" />}
-        </div>
+    <div ref={messageRef} className={`flex gap-6 ${message.senderId === currentUser.uid ? 'justify-end' : 'justify-start'}`}>
+      <div className='max-w-4/5 flex flex-col gap-1 text-gray-300'>
+        <img className='h-10 w-10 rounded-full' src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt="" />
+        <span>Just now</span>
+      </div>
+      <div className='flex flex-col gap-3'>
+        <p className='bg-white p-2 rounded-tl-xl rounded-br-xl'>
+          {message.text}
+        </p>
+        {message.img && <img className='w-40 h-40 rounded object-fit' src={message.img} alt="" />}
       </div>
     </div>
   );
