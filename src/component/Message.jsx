42b695f81507ba52c55db1c4734f7ae3b3f556a9
tawheedshimul <tawheedshimul@gmail.com>
2023-12-06 01:37:47 +0600

    updae

--- src/component/Message.jsx
+++ src/component/Message.jsx
@@ -1,19 +1,17 @@
 // Message.js
 import React, { useContext } from 'react';
-import { AuthContex } from '../context/AuthContext'; // Corrected typo in the import statement
+import { AuthContex } from '../context/AuthContext';
 import { ChatContex } from '../context/ChatContext';
 
 function Message({ message }) {
-  const { currentUser } = useContext(AuthContex); // Corrected typo in the variable name
+  const { currentUser } = useContext(AuthContex); 
   const { data } = useContext(ChatContex);
 
-  let datas = message.message
-  console.log(datas)
+  
 
   return (
     <div>
       <div className={`flex gap-6 ${message.senderId === currentUser.uid ? 'justify-end' : 'justify-start'}`}>
-        {/* Used the correct variable name `m` instead of `message` */}
         <div className='max-w-4/5 flex flex-col gap-1 text-gray-300'>
           <img className='h-10 w-10 rounded-full' src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt="" />
           <span>Just now</span>
@@ -22,7 +20,7 @@ function Message({ message }) {
           <p className='bg-white p-2 rounded-tl-xl rounded-br-xl'>
             {message.text}
           </p>
-          {message.img && <img className='w-40 h-40 rounded object-fit' src={message.img} alt="" />} {/* Added conditional rendering for image */}
+          {message.img && <img className='w-40 h-40 rounded object-fit' src={message.img} alt="" />}
         </div>
       </div>
     </div>
--- src/component/Messages.jsx
+++ src/component/Messages.jsx
@@ -1,47 +1,88 @@
-// Messages.js
-import { doc, onSnapshot, collection } from "firebase/firestore";
+// // Messages.js
+// import { doc, onSnapshot, collection } from "firebase/firestore";
+// import React, { useContext, useEffect, useState } from "react";
+// import { ChatContex } from "../context/ChatContext";
+// import { db } from "../firebase/firebase.config";
+// import Message from "./Message";
+
+// const Messages = () => {
+//   const [loading, setLoading] = useState(true);
+//   const [messages, setMessages] = useState([]);
+//   const { data } = useContext(ChatContex);
+
+//   useEffect(() => {
+//     const unsubscriber = onSnapshot(collection(db, "chats"), (snapshot) => {
+//       const messagesData = snapshot.docs.map((doc) => ({
+//         ...doc.data(),
+//         key: doc.id,
+//       }));
+//       setMessages(messagesData.flat(1)); // Use flat with a depth of 1 to flatten the array
+//       setLoading(false);
+//     });
+  
+//     return () => unsubscriber();
+//   }, []);
+  
+
+//   if (loading) {
+//     return <h1>Data is Loading...</h1>;
+//   }
+
+//   console.log(messages);
+
+//   return (
+//     <div className='bg-green-100 p-3 h-[calc(100%-130px)] overflow-y-scroll space-y-5'>
+//       {messages.length > 0 ? (
+//         messages.map((m) => <Message message={m} key={m.key} />)
+//       ) : (
+//         <p>No messages yet.</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Messages;
+
+
+import { collection, doc, onSnapshot } from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
 import { ChatContex } from "../context/ChatContext";
-import { db } from "../firebase/firebase.config";
 import Message from "./Message";
+import { db } from "../firebase/firebase.config";
 
 const Messages = () => {
-  const [loading, setLoading] = useState(true);
   const [messages, setMessages] = useState([]);
   const { data } = useContext(ChatContex);
 
   useEffect(() => {
-    const getPostsFromFirebase = [];
-    const unsubscriber = onSnapshot(collection(db, "chats"), (snapshot) => {
-      snapshot.forEach((doc) => {
-        getPostsFromFirebase.push({
-          ...doc.data(),
-          key: doc.id,
-        });
-      });
-      setMessages(getPostsFromFirebase);
-      setLoading(false);
+    const chatDocRef = doc(db, "chats", data.chatId);
+    
+    const unSub = onSnapshot(chatDocRef, (doc) => {
+      if (doc.exists()) {
+        const messagesData = doc.data()?.message || [];
+        setMessages(messagesData);
+      } else {
+        // Handle the case where the document doesn't exist
+        console.log("Document does not exist");
+        setMessages([]);
+      }
     });
-    return () => unsubscriber();
-  }, []); // Removed 'loading' from the dependency array to avoid unnecessary re-renders
 
-  if (loading) {
-    return <h1>Data is Loading...</h1>;
-  }
+    return () => {
+      unSub();
+    };
+  }, [data.chatId]);
 
-  console.log(messages)
+  console.log(messages);
 
   return (
     <div className='bg-green-100 p-3 h-[calc(100%-130px)] overflow-y-scroll space-y-5'>
-      {messages.length}
-      
-      {messages.length > 0 ? (
-        messages.map((m) => <Message message={m} key={m.id} />)
-      ) : (
-        <p>No messages yet.</p>
-      )}
+      {messages.map((m) => (
+        <Message message={m} key={m.id} />
+      ))}
     </div>
   );
 };
 
 export default Messages;
+
